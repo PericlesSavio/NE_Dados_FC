79e317435e76cb7df666a9ac680710a4d21b002e
PericlesSavio <pericles.marques@outlook.com>
2022-06-16 20:06:38 -0300

    ne2000

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## NE DADOS FC
-=======
 Link: [https://ne-dados-fc.vercel.app/](https://ne-dados-fc.vercel.app/)
 
 ### Competições
-* Copa do Nordeste: 1994-1998
+* Copa do Nordeste: 1994-2000
--- app.py
+++ app.py
@@ -310,6 +310,54 @@ def ne1998():
             empate_com_gols = pts_empate_com_gols, clube = campeao(ano, competicao).iloc[0,0]).head(1).to_dict('records'),
     )
 
+@app.route('/competicoes/ne/2000')
+def ne2000():
+    url = 'ne'
+    ano = 2000
+    competicao = 'Copa do Nordeste'
+    pts_empate_sem_gols = 1
+    pts_empate_com_gols = 1
+    pts_vitoria = 3
+
+    return render_template('/'+url+'/ne.html', #'/'+url+'/'+str(ano)+'.html',
+        url = url,
+        title = competicao,
+        edicao=ano,
+
+        n_participantes = dados(competicao, ano)['Participantes'],
+        n_partidas = dados(competicao, ano)['Nº de partidas'],
+        total_jogos = dados(competicao, ano)['Total de gols'],
+        media_gols = dados(competicao, ano)['Média de gols'],
+        periodo = dados(competicao, ano)['Período'],
+        participantes=participacoes(ano, competicao).to_dict('records'),
+
+        regulamento = 'Os 16 clubes se dividiriam em 4 grupos com 4 participantes cada. Os 2 melhores classificados de cada grupo avançariam para as quartas-de-final. Os vencedores para as semi-finais e, por fim, para a grande final. Todas as fases teriam jogos de ida e volta.',
+
+        grupo_a_classificacao = classificacao(
+            competicao, ano, 'Grupo A', 'Primeira fase', empate_sem_gols = pts_empate_sem_gols, empate_com_gols = pts_empate_com_gols).to_dict('records'),        
+        grupo_b_classificacao = classificacao(
+            competicao, ano, 'Grupo B', 'Primeira fase', empate_sem_gols = pts_empate_sem_gols, empate_com_gols = pts_empate_com_gols).to_dict('records'),        
+        grupo_c_classificacao = classificacao(
+            competicao, ano, 'Grupo C', 'Primeira fase', empate_sem_gols = pts_empate_sem_gols, empate_com_gols = pts_empate_com_gols).to_dict('records'),        
+        grupo_d_classificacao = classificacao(
+            competicao, ano, 'Grupo D', 'Primeira fase', empate_sem_gols = pts_empate_sem_gols, empate_com_gols = pts_empate_com_gols).to_dict('records'),
+
+        grupo_a_jogos = partidas(competicao, ano, 'Grupo A', 'Primeira fase').to_dict('records'),
+        grupo_b_jogos = partidas(competicao, ano, 'Grupo B', 'Primeira fase').to_dict('records'),
+        grupo_c_jogos = partidas(competicao, ano, 'Grupo C', 'Primeira fase').to_dict('records'),
+        grupo_d_jogos = partidas(competicao, ano, 'Grupo D', 'Primeira fase').to_dict('records'),
+
+        qf=partidas(competicao, ano, 0, 'Quartas de final').to_dict('records'),
+        sf=partidas(competicao, ano, 0, 'Semifinal').to_dict('records'),
+        final=partidas(competicao, ano, 0, 'Final').to_dict('records'),
+        
+        campeao = campeao(ano, competicao).to_dict('records'),
+        titulos = campeao(ano, competicao)['titulos'].to_string().replace("0    ", ""),        
+        campanha = classificacao(
+            competicao = competicao, ano = ano, grupo = 0, fase = 0, vitoria = pts_vitoria, empate_sem_gols = pts_empate_sem_gols,
+            empate_com_gols = pts_empate_com_gols, clube = campeao(ano, competicao).iloc[0,0]).head(1).to_dict('records'),
+    )
+
 
 
 
