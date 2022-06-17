92e11e02011e959aad9291f20ccda3c0658d0300
PericlesSavio <pericles.marques@outlook.com>
2022-06-17 19:13:10 -0300

    ne2001

--- app.py
+++ app.py
@@ -358,7 +358,42 @@ def ne2000():
             empate_com_gols = pts_empate_com_gols, clube = campeao(ano, competicao).iloc[0,0]).head(1).to_dict('records'),
     )
 
+@app.route('/competicoes/ne/2001')
+def ne2001():
+    url = 'ne'
+    ano = 2001
+    competicao = 'Copa do Nordeste'
+    pts_empate_sem_gols = 1
+    pts_empate_com_gols = 1
+    pts_vitoria = 3
 
+    return render_template('/'+url+'/ne.html', #'/'+url+'/'+str(ano)+'.html',
+        url = url,
+        title = competicao,
+        edicao=ano,
+
+        n_participantes = dados(competicao, ano)['Participantes'],
+        n_partidas = dados(competicao, ano)['Nº de partidas'],
+        total_jogos = dados(competicao, ano)['Total de gols'],
+        media_gols = dados(competicao, ano)['Média de gols'],
+        periodo = dados(competicao, ano)['Período'],
+        participantes=participacoes(ano, competicao).to_dict('records'),
+
+        regulamento = 'Todos os 16 clubes se enfrentariam em jogos apenas de ida, os 4 melhores classificados avançariam às semifinais, com 2 chegando à final e decidindo o título.',
+
+        primeira_fase_jogos = partidas(competicao, ano, 0, 'Primeira fase').to_dict('records'),
+        primeira_fase_classificacao = classificacao(
+            competicao, ano, 0, 'Primeira fase', empate_sem_gols = pts_empate_sem_gols, empate_com_gols = pts_empate_com_gols).to_dict('records'),
+
+        sf=partidas(competicao, ano, 0, 'Semifinal').to_dict('records'),
+        final=partidas(competicao, ano, 0, 'Final').to_dict('records'),
+        
+        campeao = campeao(ano, competicao).to_dict('records'),
+        titulos = campeao(ano, competicao)['titulos'].to_string().replace("0    ", ""),        
+        campanha = classificacao(
+            competicao = competicao, ano = ano, grupo = 0, fase = 0, vitoria = pts_vitoria, empate_sem_gols = pts_empate_sem_gols,
+            empate_com_gols = pts_empate_com_gols, clube = campeao(ano, competicao).iloc[0,0]).head(1).to_dict('records')
+    )
 
 
 
